49f9718d972d3225f5189414ef8405951bf48214
fuga11 <cikiblak35@gmail.com>
2025-05-03 22:58:52 +0300

    Update city and date display in main HTML and integrate data management functions

--- src/dataManager.js
+++ src/dataManager.js
@@ -1,10 +1,20 @@
 export function GetCity(data){
   const city = data.name;
+  return city;
   console.log(city);
 }
 export function GetDate(data){
   const date = new Date(data.dt * 1000);
   const arr = date.toString().split(" ");
   const res = arr[0] + ' '+'|'+' '+arr[1];
+  return res;
   console.log(res);
+}
+export function DateCityHTMLManager(data){
+  const date = GetDate(data);
+  const city = GetCity(data);
+  let html_c = `<p style="margin:0;"> ${city} </p>`;
+  let html_d = `<p style="margin:0;"> ${date} </p>`;
+  document.querySelector('.city-name').innerHTML = html_c;
+  document.querySelector('.date-info').innerHTML = html_d;
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { response } from "./responce.js";
-import { GetCity,GetDate } from "./dataManager.js";
+import { GetCity,GetDate,DateCityHTMLManager } from "./dataManager.js";
 response()
   .then(data => {
     console.log('Data:', data);
     GetCity(data);
     GetDate(data);
+    DateCityHTMLManager(data);
   })
   .catch(error => {
     console.log('Error:', error);
